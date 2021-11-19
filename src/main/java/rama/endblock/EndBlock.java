29a9be20be4a5368838f74d484ae630a5b448afe
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 13:30:55 -0300

    Initial commit

---
+++ src/main/java/rama/endblock/EndBlock.java
@@ -0,0 +1,17 @@
+package rama.endblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
